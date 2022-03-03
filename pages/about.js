74d78c093daa19d659426abb247f1a89d58d7bf7
chriscashen <chris.cashen@hotmail.com>
2022-03-03 08:49:17 -0800

    initial

---
+++ pages/about.js
@@ -0,0 +1,25 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Incididunt labore cillum est tempor magna labore labore mollit
+          eiusmod.
+        </p>
+        <p>
+          Incididunt labore cillum est tempor magna labore labore mollit
+          eiusmod.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
